2466f616dcfa68c11c9a288361a19b743fd678a9
leannachan12 <leannachan1210@gmail.com>
2024-09-23 17:26:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from dash import Dash, html, dcc, callback, Input, Output
 import numpy as np 
 import pandas as pd 
 import plotly.express as px
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
 app.title = "MCM7183 Exercise 3"
 server = app.server
 
 df = pd.read_csv("https://raw.githubusercontent.com/wenjiun/MCM7183Exercise3/main/assets/gdp_1960_2020.csv")
-
-
 
 image_path = 'assets/logo-mmu.png'
 
@@ -17,16 +16,17 @@ app.layout = [html.H1('MCM7183 Exercise 3'),
               html.Img(src=image_path), 
               dcc.Dropdown(['Malaysia', 'Indonesia', 'China'], 'Malaysia', id='dropdown-country'),
               dcc.Graph(id="graph-scatter"), 
-              dcc.Dropdown([{'label':'2020', 'value':2020}, {'label':'2010', 'value':2010}, 
-                            {'label':'2000', 'value':2000}], 2020, id='dropdown-year'),
-                dcc.Slider(1960,2020,5,value=2020, id='slider-year'),
+              #dcc.Dropdown([{'label':'2020', 'value':2020}, {'label':'2010', 'value':2010}, 
+              #              {'label':'2000', 'value':2000}], 2020, id='dropdown-year'),
+              dcc.Slider(1960, 2020, 5, value=2020, id='slider-year',
+                         marks = {i: str(i) for i in range(1960, 2021, 5)}),
               dcc.Graph(id="graph-pie")]
 
 @callback(
     Output('graph-scatter', 'figure'),
     Output('graph-pie', 'figure'),
     Input('dropdown-country', 'value'),
-    Input('slider-year', 'value'),
+    Input('slider-year', 'value')
 )
 def update_graph(country_selected, year_selected):
     subset_Country = df[df['country'].isin([country_selected])]
@@ -42,8 +42,11 @@ def update_graph(country_selected, year_selected):
     mylabels = ["Asia", "Africa", "America", "Europe","Oceania"]
     pie_df = {'Continent': mylabels,'GDP': pie_data}
     fig2 = px.pie(pie_df,values="GDP",names="Continent")
+    fig2.update_traces(sort=False) 
 
     return fig, fig2
+
+
 
 
 if __name__ == '__main__':
